01aa14e2a7f9a4d99bf4ee610aa3d662f1f00206
Demetrius <dlkattes@hotmail.com>
2026-04-28 16:57:19 -0400

    Update fixtures and pages for home test

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -4,7 +4,7 @@ import { Page } from '@playwright/test';
 export class BasePage {
   constructor(protected page: Page) {}
 
-  async goto(path: string) {
-    await this.page.goto(path);
-  }
+  async goto(path = '/home.html') {
+  await this.page.goto(`file://${process.cwd()}/static${path}`);
+ }
 }
--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -2,6 +2,10 @@
 import { BasePage } from './BasePage';
 
 export class HomePage extends BasePage {
+  async goto(path = '/') {
+    await super.goto(path);
+  }
+
   async getTitle() {
     return this.page.title();
   }
