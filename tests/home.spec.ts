30773b5c9300d953a91c3f621a189824bf49e6c4
Demetrius Kattes <dlkattes@hotmail.com>
2026-05-13 19:25:55 -0400

    home page test path edited

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,7 +2,7 @@
 import { test, expect } from '../fixtures/homeFixture';
 
 test('homepage loads and has title', async ({ homePage }) => {
-  await homePage.goto('/');
+  await homePage.goto('/home.html');
   const title = await homePage.getTitle();
   expect(title).toContain('Playwright');
 });
